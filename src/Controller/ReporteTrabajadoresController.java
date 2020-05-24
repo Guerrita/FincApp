31f01401cf668d9a92d2fc766fd976c2355e3abc
Alejandro_CristanchoM <alejandro.cristancho@udea.edu.co>
2020-05-23 22:42:17 -0500

    Se consolida la funcionalidad de mostrar un reporte de los trabajadores

--- src/Controller/ReporteTrabajadoresController.java
+++ src/Controller/ReporteTrabajadoresController.java
@@ -2,9 +2,15 @@ package Controller;
 
 import Bsn.TrabajadorBsn;
 import Model.Trabajador;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+
+import java.util.List;
 
 
 public class ReporteTrabajadoresController {
@@ -22,10 +28,17 @@ public class ReporteTrabajadoresController {
     @FXML
     TableColumn<Trabajador, String> clmFechaRegistro;
 
-    private TrabajadorBsn trabajadorbsn;
+    private TrabajadorBsn trabajadorbsn=new TrabajadorBsn();
 
     @FXML
     private void initialize(){
-        //todo llenar lista de trabajadores, crear un botón para que la cree
+        List<Trabajador> trabajadores= trabajadorbsn.listarTrabajadores();
+        ObservableList<Trabajador> trabajadorObservables = FXCollections.observableList(trabajadores);
+        clmId.setCellValueFactory(new PropertyValueFactory<>("id"));
+        clmNombres.setCellValueFactory(new PropertyValueFactory<>("nombres"));
+        clmApellidos.setCellValueFactory(new PropertyValueFactory<>("apellidos"));
+        clmCelular.setCellValueFactory(new PropertyValueFactory<>("celular"));
+        clmFechaRegistro.setCellValueFactory(new PropertyValueFactory<>("fechaDeRegistro"));
+        tblTrabajadores.getItems().addAll(trabajadorObservables);
     }
 }
